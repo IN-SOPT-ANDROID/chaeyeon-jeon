9109757db5cd1cb75ef46e228d893b2936a9a8ea
b1urrrr <b1urrr@naver.com>
2022-10-04 06:26:17 +0900

    [FEAT/#1] 자기소개 페이지 만들기

--- app/src/main/java/org/sopt/sample/HomeActivity.kt
+++ app/src/main/java/org/sopt/sample/HomeActivity.kt
@@ -2,10 +2,22 @@ package org.sopt.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.sopt.sample.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 회원정보 받아오기
+        if (intent.hasExtra("id")) {
+            binding.txtName.text = binding.txtName.text.toString() + intent.getStringExtra("id");
+        }
+        if (intent.hasExtra("mbti")) {
+            binding.txtMbti.text = binding.txtMbti.text.toString() + intent.getStringExtra("mbti");
+        }
     }
 }
